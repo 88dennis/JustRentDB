5cb2f8b91b17d2c81a79aec79ac99f0a7a251cb1
Dennis Sarmiento <dennissarmiento8080@gmail.com>
2020-04-07 16:39:36 +0800

    up

--- app.js
+++ app.js
@@ -2,21 +2,58 @@ let express = require("express");
 let bodyParser = require("body-parser");
 let PORT = 3000;
 let app = express();
+let mongoose = require("mongoose");
 
-let rentalObjects = [
-    { name: "Generator Set", description: "10KVA", price: 1200, imgSource: "/images/generator.jpg" },
-    { name: "Industrial fan", description: "Stand Fan", price: 1900, imgSource: "https://i.imgur.com/vcqEmRW.jpg" },
-    { name: "Air cooler", description: "fan coolers", price: 2500, imgSource: "/images/aircooler.jpg" },
-    { name: "Jack Hammer", description: "Portable Jackhammer", price: 800, imgSource: "https://i.imgur.com/SyuUX9F.jpg" },
-    { name: "Welding Machine", description: "Portable Welding Machine", price: 900, imgSource: "https://i.imgur.com/EF8eRoG.jpg" },
-    { name: "Coring Machine", description: "Portable Coring Machine", price: 5000, imgSource: "/images/coringmachine.jpg" },
-];
+// let rentalObjects = [
+//     { name: "Generator Set", description: "10KVA", price: 1200, imgSource: "/images/generator.jpg" },
+//     { name: "Industrial fan", description: "Stand Fan", price: 1900, imgSource: "https://i.imgur.com/vcqEmRW.jpg" },
+//     { name: "Air cooler", description: "fan coolers", price: 2500, imgSource: "/images/aircooler.jpg" },
+//     { name: "Jack Hammer", description: "Portable Jackhammer", price: 800, imgSource: "https://i.imgur.com/SyuUX9F.jpg" },
+//     { name: "Welding Machine", description: "Portable Welding Machine", price: 900, imgSource: "https://i.imgur.com/EF8eRoG.jpg" },
+//     { name: "Coring Machine", description: "Portable Coring Machine", price: 5000, imgSource: "/images/coringmachine.jpg" },
+// ];
 // console.log(rentalObjects);
-console.log(rentalObjects[0]["name"]);
+// console.log(rentalObjects[0]["name"]);
+
+//connect to the database
+//if you dont have a database this will create one for you
+//when you run mongoose connect this will try to find the bird_app then if it did not find it will create one
+//from mongoose.com documentation
+//the birdApp is the name of our database
+mongoose.connect('mongodb://localhost:27017/justRentDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+
+//SCHEMA
+let justRentSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    price: Number,
+    imgSource: String
+});
+
+//COMPILE THE SCHEMA INTO A MODEL
+//the Arguement "Rental" will automatically give you a collection named "Rental"
+//this model pluralizes the "Rental" automatically and create a collection rentals
+//example: db.rentals
+//making a model out of the schema so we can use the methods
+let Rental = mongoose.model("Rental", justRentSchema);
+
+// Rental.create(
+//     { name: "Welding Machine", description: "Portable Welding Machine", price: 900, imgSource: "https://i.imgur.com/EF8eRoG.jpg" },
+//     function(err, rental){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log("added ITEMS!!!!")
+//             console.log(rental);
+//         }
+//     }
+// );
+
 
 //Route for the landingPage.ejs
 app.get("/", function (req, res) {
@@ -25,14 +62,20 @@ app.get("/", function (req, res) {
 
 //Route for the viewRentalsPage.ejs - where the items are displayed
 app.get("/forRentItems", function (req, res) {
-    console.log("*******************************");
-    console.log(rentalObjects);
-    res.render("viewRentalsPage", { rentalObjectsEjs: rentalObjects })
+    //GET ALL ITEMS FOR RENT FROM THE DATABASE USING MONGOOSE SYNTAX REFERRING TO THE MONGOOSE MODEL
+    Rental.find({}, function (err, allRental) {
+        if (err) {
+            console.log(err)
+        } else {
+            //RENDER THE OBJECT FROM THE DATABASE
+            console.log(allRental);
+            res.render("viewRentalsPage", { rentalObjectsEjs: allRental })
+        }
+    })
 });
 
 //Route for the Page new.ejs - where you add new items
 app.get("/forRentItems/new", function (req, res) {
-
     res.render("new");
 });
 
